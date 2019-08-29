c85867be4b4580b8f28df8cea42817b4bb364681
Pedro <pedro.lazari@fcamara.com.br>
2019-08-29 00:33:19 -0300

    Import Database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Seja bem vindo ao GoBarber' });
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'peDRO LAZAZRI',
+    email: 'pedro.lazari@skmd.com',
+    password_hash: 'isdjdjads',
+  });
+  return response.json({ message: `User ${user.id} criado com sucesso` });
 });
 
 // module.exports = routes;
